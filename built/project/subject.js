f031d8d530c4994b8086bb62b0757bc6b7a62035
simonfan <simonxd@gmail.com>
2013-12-15 02:48:41 -0200

    add proto method and modify extend method internal workflow. API same

--- built/project/subject.js
+++ built/project/subject.js
@@ -2,4 +2,4 @@
 //     (c) simonfan
 //     subject is licensed under the MIT terms.
 
-define(["lodash"],function(e){var t={initialize:function(){},extend:function(){var t=Array.prototype.slice.call(arguments);return t.unshift(this),e.extend.apply(e,t),this}},n=function(){return Object.create(t)};return n.base=t,n.extend=function(){var t=Object.create(this.base);t.extend.apply(t,arguments);var n=function(){var n=Object.create(t);return n.initialize.apply(n,arguments),n};return n.base=t,n.extend=this.extend,n.define=n.extend,n},n.define=n.extend,n});
+define(["lodash"],function(e){function t(e){return Array.prototype.slice.call(e)}var n={initialize:function(){}},r=function(){return Object.create(n)};return r.prototype=n,r.inherit=function(n){return e.assign(this.prototype,n),this},r.extend=function(n,r){var i=this,s;return s=function(){var t=Object.create(s.prototype);return t.initialize.apply(t,arguments),t},e.assign(s,i,r),s.prototype=Object.create(this.prototype),s.prototype.constructor=s,s.inherit(n),s},r});
