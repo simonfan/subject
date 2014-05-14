695b3527ba15b640192ed009d96769e916b08f4f
simonfan <simonxd@gmail.com>
2014-05-14 14:42:16 -0300

    assign takes accessor descriptors as well

--- src/__subject/private/assign.js
+++ src/__subject/private/assign.js
@@ -8,12 +8,86 @@ define(function (require, exports, module) {
 
 	var _ = require('lodash');
 
+		// descriptor for both accessor and data
 	var defaultDescriptor = {
-	//	value:
-		configurable: true,
-		writable:     true,
-		enumerable:   true,
-	};
+			configurable: true,
+			enumerable:   true,
+		},
+		// descriptor for data only.
+		defaultDataDescriptor = _.extend({
+			writable:     true,
+		}, defaultDescriptor);
+
+
+
+	/**
+	 * Defines properties using accessor descriptor
+	 *
+	 * @method assignAccessor
+	 * @param  {[type]} obj        [description]
+	 * @param  {[type]} properties [description]
+	 * @param  {[type]} descriptor [description]
+	 * @return {[type]}            [description]
+	 */
+	function assignAccessor(obj, properties, descriptor) {
+
+		// when the descriptor DOES NOT HAVE
+		// neither 'get' nor 'set'
+		// set defaults for configurable, enumerable and writable.
+		_.defaults(descriptor, defaultDescriptor);
+
+		if (_.isArray(properties)) {
+
+			_.each(properties, function (property) {
+
+				// partialize get and set
+				// using the property name as first argument.
+				var desc = _.extend({}, descriptor);
+				if (desc.get) {
+					desc.get = _.partial(desc.get, property);
+				}
+
+				if (desc.set) {
+					desc.set = _.partial(desc.set, property);
+				}
+
+				// run defineProperty
+				Object.defineProperty(obj, property, desc);
+			});
+
+		} else {
+
+			// TODO
+			throw new Error('Currently subject.assign does not accept non-array properties for accessor assignment.');
+		}
+	}
+
+	/**
+	 * Defines properties using data descriptors.
+	 *
+	 * @method assignData
+	 * @param  {[type]} obj        [description]
+	 * @param  {[type]} data [description]
+	 * @param  {[type]} descriptor [description]
+	 * @return {[type]}            [description]
+	 */
+	function assignData(obj, data, descriptor) {
+
+		// when the descriptor HAS
+		// either 'get' or 'set'
+		// set defaults only for configurable, enumerable
+		_.defaults(descriptor, defaultDataDescriptor);
+
+
+		_.each(data, function (value, property) {
+
+			// set value on descriptor
+			var desc = _.assign({ value: value }, descriptor);
+
+			// run defineProperty
+			Object.defineProperty(obj, property, desc);
+		});
+	}
 
 	/**
 	 *
@@ -21,35 +95,30 @@ define(function (require, exports, module) {
 	 * @method assign
 	 * @private
 	 * @param obj
-	 * @param extensions
+	 * @param data
 	 * @param [descriptor]
 	 */
-	module.exports = function assign(obj, extensions, descriptor) {
+	module.exports = function assign(obj, data, descriptor) {
 
 		if (!descriptor) {
 			// simple assigning.
-
-			return _.assign(obj, extensions);
+			_.assign(obj, data);
 
 		} else {
 			// use defineProperty to assign.
 
-			// set default values for descriptor
-			_.defaults(descriptor, defaultDescriptor);
+			if (descriptor.get || descriptor.set) {
+				// ACCESSOR DESCRIPTOR
 
-			_.each(extensions, function (value, property) {
+				assignAccessor(obj, data, descriptor);
 
-				// set value on descriptor
-				var desc = _.assign({ value: value }, descriptor);
-
-			//	console.log('define ' + property);
-
-				// run defineProperty
-				Object.defineProperty(obj, property, desc);
-			});
-
-			return obj;
+			} else {
+				//
+				assignData(obj, data, descriptor);
+			}
 		}
+
+		return obj;
 	};
 
 });
